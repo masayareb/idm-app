d22bfc5da228ae428b743b3b99c4b4369ad37f1b
masayareb <masaya.1021.joho@icloud.com>
2023-05-09 09:24:32 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #バリデーション
+  validates :name, presence: true
+  validates :affiliation, presence: true
+  validates :admin, presence: true
+  #半角6~12文字で英大文字・英小文字・数字それぞれ１文字以上含む
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/, message: "Password is invalid. Include both letters and numbers"}
+  #半角英数字のみ
+  validates :employee_number, presence: true, format: { with:/\A[a-z0-9]+\z/i, message: "Employee_Number is invalid."}
+
 end

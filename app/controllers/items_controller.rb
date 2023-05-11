10c017c531f4d5a4dde94ad1b8327ba040d5203a
masayareb <masaya.1021.joho@icloud.com>
2023-05-11 18:53:53 +0900

    機器登録用コントローラーを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :authenticate_user!, except: [:show, :index]
+  def index 
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create 
+    @item = Item.create(item_params)
+    if  @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :category_id, :item, :status_id, :manufacturer, :serial_number, :ip, :purchase_date, :years_of_use,:remarks).merge(user_id: current_user.id)
+   end
 end

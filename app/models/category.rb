52116a9aedad665edb05a25f8f5842857d82dc56
masayareb <masaya.1021.joho@icloud.com>
2023-05-11 21:54:55 +0900

    stetus項目の表示を変更（軽微）

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,11 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '10.デスクトップPC' },
-    { id: 2, name: '11.ノートPC' },
-    { id: 3, name: '20.複合機' },
-    { id: 4, name: '30.Ipad' },
-    { id: 5, name: '40.Wi-Fiルータ' },
-    { id: 6, name: '41.モバイルルータ' }
+    { id: 1, name: '1.デスクトップPC' },
+    { id: 2, name: '2.ノートPC' },
+    { id: 3, name: '10.複合機' },
+    { id: 4, name: '20.Ipad' },
+    { id: 5, name: '30.Wi-Fiルータ' },
+    { id: 6, name: '31.モバイルルータ' }
   ]
   include ActiveHash::Associations
   has_many :items

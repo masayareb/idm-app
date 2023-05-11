dcb1d5d7d9c35a22694417b0e7f2b1330e46465a
masayareb <masaya.1021.joho@icloud.com>
2023-05-11 21:08:28 +0900

    シリアルナンバーをString型へ変更

--- db/migrate/20230510042711_create_items.rb
+++ db/migrate/20230510042711_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string  :item ,null: false #商品名
       t.integer :category_id ,null: false #カテゴリー
       t.string :manufacturer ,null: false #メーカー
-      t.integer :serial_number ,null: false,unique: true #S/N
+      t.string :serial_number ,null: false,unique: true #S/N
       t.string :ip ,null: false #IPアドレス
       t.date :purchase_date ,null: false #購入年月
       t.integer :years_of_use ,null: false #利用予定年数

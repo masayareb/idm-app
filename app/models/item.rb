e25c1f6f972f902a39587c8709faa6ba66ee2a6f
masayareb <masaya.1021.joho@icloud.com>
2023-05-11 21:51:59 +0900

    バリデーション修正（imageを任意登録へ）

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,14 +17,13 @@ class Item < ApplicationRecord
   validates :status_id, presence: true 
   validates :item, presence: true
   validates :category_id, presence: true
-  validates :image, presence: true
   validates :manufacturer, presence: true
   #S/N英数字のみ
   validates :serial_number, presence: true,uniqueness: true, format: { with: /\A[A-Za-z0-9]+\z/ }
   #192.168.000.000形式もしくはDHCP
   validates :ip, presence: true, format: { with: Resolv::IPv4::Regex }
   #yyyy-mm-dd形式
-  validates :purchase_date, presence: true, format: { with: /\A\d{4}-\d{3}-\d{2}\z/ }
+  validates :purchase_date, presence: true
   #y年表示
-  validates :years_of_use, presence: true, format: { with: /\A[0-9]+\z/ }
+  validates :years_of_use, presence: true
 end

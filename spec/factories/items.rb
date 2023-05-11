d259c551aa8dc0b25e46ff360006440d6c6bba2b
masayareb <masaya.1021.joho@icloud.com>
2023-05-11 21:52:11 +0900

    単体テスト実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item                {'test'}
+    manufacturer        {'メーカー'}
+    category_id         {10}
+    status_id           {2}
+    serial_number       {'a1111111'}
+    ip                  {'192.168.100.1'}
+    purchase_date       {'2023-10-21'}
+    years_of_use        {4}
+		remarks             {"テストテストテストテスト"}
+		association :user
+    #afterメソッドを用いてダミー画像を添付
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end

e44b9edf5acf21636b91f3b99b397e114ed3a3bb
masayareb <masaya.1021.joho@icloud.com>
2023-05-12 10:20:58 +0900

    購入年月が古い順に表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   def index 
+    @items = Item.order(purchase_date: :asc)
   end
 
   def new

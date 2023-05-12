e54449b6b4ba0c11ef3560c4d9ae076c9d508059
masayareb <masaya.1021.joho@icloud.com>
2023-05-12 15:36:47 +0900

    edit,updateアクションを記述（リファクタリング、エラーハンドリング等）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,17 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_item, only: [:edit, :show, :update]
+
   def index 
     @items = Item.order(purchase_date: :asc)
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+
   end
 
   def create 
@@ -17,9 +23,28 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :category_id, :item, :status_id, :manufacturer, :serial_number, :ip, :purchase_date, :years_of_use,:remarks).merge(user_id: current_user.id)
-   end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
 end

d0a66f8b2cd689a3bf2772f5fd1e96d7c4314989
masayareb <masaya.1021.joho@icloud.com>
2023-05-17 14:32:23 +0900

    コントローラー追記ユーザ情報編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
   before_action :check_admin
+  before_action :set_user, only: [:edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.order("employee_number ASC")
   end
 
   def edit
+
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  
   end
 
   def destroy
@@ -23,6 +31,10 @@ class UsersController < ApplicationController
     unless current_user&.admin?
       redirect_to root_path
     end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end

71d5796cc4184f7d2cbfb706231829354802a2ca
masayareb <masaya.1021.joho@icloud.com>
2023-05-18 20:27:32 +0900

    userモデルへアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   #バリデーション
   validates :name, presence: true
   validates :affiliation, presence: true

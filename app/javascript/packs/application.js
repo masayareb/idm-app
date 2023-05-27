1c0b99224940b5459eb85b2c9774b2f50aab28b7
masayareb <masaya.1021.joho@icloud.com>
2023-05-27 16:19:53 +0900

    新規ユーザ登録の入力時の条件を設定
    入力条件に適した値で入力しないとエラーが発生
    全ての項目の入力条件をクリアすると保存ボタンがアクティブになる。
    ※ユニークな値やパスワードが正しいかをみていないので登録を押してもバリデーションでエラーが発生することはある。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("../item_show")
 require("../item_input")
+require("../user_input")
+require("../user_edit_input")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

593f4122fbc8efcf98288ffe4ec0f2828ecac9b4
masayareb <masaya.1021.joho@icloud.com>
2023-06-17 15:42:34 +0900

    モデルへ取得したファイルからデータを保存
    idの有無によって新規登録もしくはデータ更新
    社員番号をuser_idへ書き換え
    エラー件数をカウント

--- app/models/item.rb
+++ app/models/item.rb
@@ -28,4 +28,48 @@ class Item < ApplicationRecord
   validates :years_of_use, presence: true
 
 
+  #csv_import
+  def self.import(file)
+    all_count = 0
+    failure_id = []
+    failure_count = 0
+    registration_count = 0
+    alert = []
+    CSV.foreach(file.path, headers: true) do |row|
+      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+      item = find_by(id: row["id"]) || new
+      # 社員番号からuser.idを割り出す
+      item.user = User.find_by(employee_number: row["社員番号"])
+      # CSVからデータを取得し、設定する
+      item.attributes = updatable_attributes(row)
+      all_count += 1
+      # 保存する
+      if item.save
+        registration_count += 1
+      else
+        failure_count += 1
+        failure_id << row["id"]
+      end
+    end
+
+    if failure_count == 0
+      alert << "取り込み実施件数：#{all_count}件中、全件取り込み成功!"
+      return alert
+      #redirect_to items_import_page_path
+    else
+      alert << "取り込み実施件数：#{all_count}件中、取り込み失敗件数：#{failure_count}"
+      failure_id.each do |id|
+        alert << "取り込みエラー 対象id:#{id}"
+      end 
+
+      return alert
+    end
+
+  end
+
+  def self.updatable_attributes(row)
+    {id: row["id"], status_id: row["状態"], item: row["機器名"], category_id: row["機器種類"], manufacturer: row["メーカー名"], serial_number: row["S/N"], ip: row["IPアドレス"], purchase_date: row["購入年月"], years_of_use: row["利用予定年数"], remarks: row["備考"]}
+  end
+
+
 end
